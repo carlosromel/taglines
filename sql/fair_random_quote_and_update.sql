c4a4c0af79a5b07e8c3245b3011dd8c9c09ce150
Carlos Romel Pereira da Silva <carlos.romel@gmail.com>
2017-11-26 03:01:03 -0200

    mend

---
+++ sql/fair_random_quote_and_update.sql
@@ -0,0 +1,10 @@
+﻿   update tagline t
+      set usage_count = x.new_usage_count
+     from (select t.tagline_id, t.usage_count + 1
+             from tagline t
+            where t.usage_count = (select min(usage_count)
+                                     from tagline)
+         order by random()
+            limit 1) x (id, new_usage_count)
+    where t.tagline_id = x.id
+returning t.tag;
